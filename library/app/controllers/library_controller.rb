ed605cb91cbec7c61cc0f6c820341f93ae00dda1
key71004 <key71004@gmail.com>
2017-05-04 23:51:50 -0400

    finish edit book feature

--- library/app/controllers/library_controller.rb
+++ library/app/controllers/library_controller.rb
@@ -6,3 +6,25 @@ class LibraryController < ApplicationController
       @book = Book.new
     end
 end
+ def create
+    @book=Book.new(candidates_params)
+    if @book.save
+      redirect_to book_path, notice: "新增成功"
+    else
+      render 'new'
+      #redirect_to new_candidates_path
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    @book =find_book()
+    
+    if @book.update(books_params)
+      redirect_to books_path, notice: "更改成功"
+    else
+      render 'edit'
+    end
+  end
